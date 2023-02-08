5f96ec35fb5cd6bb394ba552d72b399c2be1b255
Will58727 <william.powell501@yahoo.com>
2023-02-07 20:59:41 -0600

    2/7/23 updates

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ let app = express();
 
 console.log("Hello World");
 console.log(process.env.MENU_API_KEY);
-
+app.use(logMiddleware);
 //syntax for routes in express
     //res.send('response string');
     // function home(req, res) {
@@ -17,6 +17,11 @@ console.log(process.env.MENU_API_KEY);
     // app.get("/heading", (req, res) => res.send("<h3 style={{ color: 'blue' }}>Hello World</h3>"));
 
 
+
+
+    // change new assets to the same code as the index.html
+
+
 const pathToIndex = __dirname + "/views/index.html"
 app.get('/', (req, res) => res.sendFile(pathToIndex));
 
@@ -24,11 +29,47 @@ const pathToAssets = __dirname + "/public"
 app.use('/public', express.static(pathToAssets));
 
 const pathToAbout = __dirname + "/views/about.html"
-app.use('/about', express.static(pathToAbout));
+app.get('/about', (req, res) => res.sendFile(pathToAbout));
 
 const pathToContact = __dirname + "/views/contact.html"
-app.use('/contact', express.static(pathToContact));
+app.get('/contact', (req, res) => res.sendFile(pathToContact));
 
+const pathToRed = __dirname + "/views/red.html"
+app.get('/red', (req, res) => res.sendFile(pathToRed));
+
+const pathToBlue = __dirname + "/views/blue.html"
+app.get('/blue', (req, res) => res.sendFile(pathToBlue));
+
+
+
+app.get("/color", (req, res) =>{
+
+    if (process.env.BG_COLOR == "red"){
+        res.sendFile(pathToRed);
+    }else{(process.env.BG_COLOR == "blue")
+         res.sendFile(pathToBlue);
+    }
+});
+
+function logMiddleware(req, res, next) {
+    console.log(`${req.method} request was made to path: ${req.path} by IP: ${req.ip}`);
+    next();
+}
+
+//authentication Middleware
+function authenticationMiddleware(req, res, next) {
+    const token = req.header("Authorization");
+    if (token == "secret-token") {
+        next();
+    } else {
+        res.status (401).send("Unauthorized");
+    }
+}
+
+
+
+
+console.log(process.env.ENV_VARIABLE)
 
 
 app.get("/json", (req, res) =>{
@@ -48,7 +89,10 @@ app.get("/json", (req, res) =>{
     res.json(jsonObj)
 });
 
-
+app.get("/users/:username", authenticationMiddleware, (req, res) => {
+    const username = req.params.username;
+    res.send(`Hello ${username}`);
+})
 
 app.get('/json', (req, res) => {res.json(jsonObj)});
 
